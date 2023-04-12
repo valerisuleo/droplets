28afdc0cd97a0aecc0fa14546f7a2539c0e4a445
valerisuleo <valerio.risuleo@gmail.com>
2023-04-12 23:26:31 +0100

    refactor navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,35 +1,64 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
+const navLinks = [
+    {
+        path: '/building-components',
+        text: 'Building Components',
+    },
+    {
+        path: '/managing-components-state',
+        text: 'Managing Components State',
+    },
+    {
+        path: '/movies',
+        text: 'React Http',
+    },
+    {
+        path: '/movies/new',
+        text: 'Forms',
+    },
+];
+
 const Navbar = () => {
+    const [show, setShow] = useState(false);
+
+    const handleToggle = () => setShow(!show);
+
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-expand-md navbar-primary bg-primary">
             <div className="container-fluid">
-                <div>
-                    <span className='me-3'>
-                        <NavLink to={'/building-components'}>
-                            Building Components
-                        </NavLink>
-                    </span>
-                    <span className='me-3'>
-                        <NavLink to={'/managing-components-state'}>
-                            Managing Components State
-                        </NavLink>
-                    </span>
-                    <span className='me-3'>
-                        <NavLink to={'/movies'}>
-                            React Http
-                        </NavLink>
-                    </span>
-                    <span className='me-3'>
-                        <NavLink to={'/movies/new'}>
-                            Forms
-                        </NavLink>
-                    </span>
-                    <span className='me-3'>
-                        <NavLink to={'/home'}>
-                            home
-                        </NavLink>
-                    </span>
+                <span className="navbar-brand mb-0 h1 text-white">
+                    Navbar
+                </span>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={handleToggle}
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className={`collapse navbar-collapse justify-content-end ${
+                        show ? 'show' : ''
+                    }`}
+                    id="navbarNav"
+                >
+                    <ul className="navbar-nav">
+                        {navLinks.map((link) => (
+                            <li className="nav-item" key={link.path}>
+                                <NavLink
+                                    className="nav-link text-white"
+                                    to={link.path}
+                                >
+                                    {link.text}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </nav>
